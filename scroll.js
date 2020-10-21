e020b5d62cd7133b66a9e65ba7b137796f32b869
misssffit <misssffit@gmail.com>
2020-10-21 13:12:18 +0300

    Add all

---
+++ scroll.js
@@ -0,0 +1,15 @@
+function backtoTop(){
+	let button = $('.scroll');
+	$(window).on('scroll', () => {
+		if ($(this).scrollTop() >= 50) {
+			button.fadeIn();
+		} else {
+			button.fadeOut();
+		}
+	});
+	button.on('click', (e) => {
+		e.preventDefault();
+		$('html').animate({scrollTop: 0}, 1000);
+	})
+}
+backtoTop();
